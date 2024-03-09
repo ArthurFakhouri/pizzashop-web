23b63c56e2353ddeb27372056c48115a3a4884ee
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-03-09 08:04:44 -0300

    feat: Creating button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-export function App() {
+import '@/global.css'
+import { Button } from './components/ui/button'
 
+export function App() {
   return (
-    <div>
+    <div className='flex flex-col justify-center items-center h-screen'>
       <h1>Hello World</h1>
+      <Button>Send</Button>
     </div>
   )
 }
