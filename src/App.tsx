ef9965f066073cd7a605866c67cc83ed3ed9f7f7
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-03-09 08:41:57 -0300

    config: Adding and setting up react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import '@/global.css'
 
-import { Button } from './components/ui/button'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './routes'
 
 export function App() {
-  return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <h1>Hello World</h1>
-      <Button>Send</Button>
-    </div>
-  )
+  return <RouterProvider router={router} />
 }
