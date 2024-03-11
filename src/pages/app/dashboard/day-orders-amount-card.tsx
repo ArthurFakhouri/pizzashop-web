b9c1454d5a2d08115894192c9ef13d06b33c4d67
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-03-11 16:57:35 -0300

    feat: Implementing metrics

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -1,41 +1,39 @@
 import { useQuery } from '@tanstack/react-query'
-import { MapPinOff } from 'lucide-react'
+import { Utensils } from 'lucide-react'
 
-import { getMonthCanceledOrdersAmount } from '@/api/get-month-canceled-orders-amount'
+import { getDayOrdersAmount } from '@/api/get-day-orders-amount'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
-export function MonthCanceledOrdersAmountCard() {
-  const { data: monthCanceledOrdersAmount } = useQuery({
-    queryFn: getMonthCanceledOrdersAmount,
-    queryKey: ['metrics', 'month-canceled-orders-amount'],
+export function DayOrdersAmountCard() {
+  const { data: dayOrdersAmount } = useQuery({
+    queryFn: getDayOrdersAmount,
+    queryKey: ['metrics', 'day-orders-amount'],
   })
 
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-base font-semibold">
-          Cancelamento (mês)
-        </CardTitle>
-        <MapPinOff className="h-4 w-4 text-muted-foreground" />
+        <CardTitle className="text-base font-semibold">Pedidos (dia)</CardTitle>
+        <Utensils className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        {monthCanceledOrdersAmount && (
+        {dayOrdersAmount && (
           <>
             <span className="text-2xl font-bold tracking-tight">
-              {monthCanceledOrdersAmount.amount.toLocaleString('pt-BR')}
+              {dayOrdersAmount.amount.toLocaleString('pt-BR')}
             </span>
             <p className="text-xs text-muted-foreground">
-              {monthCanceledOrdersAmount.diffFromLastMonth < 0 ? (
+              {dayOrdersAmount.diffFromYesterday >= 0 ? (
                 <>
                   <span className="text-emerald-500 dark:text-emerald-400">
-                    {monthCanceledOrdersAmount.diffFromLastMonth}%
+                    +{dayOrdersAmount.diffFromYesterday}%
                   </span>{' '}
                   em relação a ontém.
                 </>
               ) : (
                 <>
                   <span className="text-rose-500 dark:text-rose-400">
-                    +{monthCanceledOrdersAmount.diffFromLastMonth}%
+                    {dayOrdersAmount.diffFromYesterday}%
                   </span>{' '}
                   em relação a ontém.
                 </>
