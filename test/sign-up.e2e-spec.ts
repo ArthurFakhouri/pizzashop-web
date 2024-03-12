fb6f2de91730f2d4f187ae4c877f9f4f0a8009bf
Arthur G. Fakhouri <71707151+ArthurFakhouri@users.noreply.github.com>
2024-03-12 15:55:16 -0300

    refactor: async assertions on E2E tests

--- test/sign-up.e2e-spec.ts
+++ test/sign-up.e2e-spec.ts
@@ -12,7 +12,7 @@ test('sign up successfully', async ({ page }) => {
 
   const toast = page.getByText('Restaurante cadastrado com sucesso')
 
-  expect(toast).toBeVisible()
+  await expect(toast).toBeVisible()
 })
 
 test('sign up with wrong credentials', async ({ page }) => {
@@ -27,9 +27,7 @@ test('sign up with wrong credentials', async ({ page }) => {
 
   const toast = page.getByText('Erro ao cadastrar restaurante')
 
-  expect(toast).toBeVisible()
-
-  await page.waitForTimeout(2000)
+  await expect(toast).toBeVisible()
 })
 
 test('navigate to new login page', async ({ page }) => {
@@ -37,5 +35,5 @@ test('navigate to new login page', async ({ page }) => {
 
   await page.getByRole('link', { name: 'Fazer login' }).click()
 
-  expect(page.url()).toContain('/sign-in')
+  await expect(page.url()).toContain('/sign-in')
 })
